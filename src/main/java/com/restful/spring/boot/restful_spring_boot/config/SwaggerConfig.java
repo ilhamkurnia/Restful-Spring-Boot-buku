f7cee70374a68a2479353d97afd0bc502b521166
ilhamkurnia <ilhammcd@gmail.com>
2019-07-18 10:25:28 +0700

    oauth2

--- src/main/java/com/restful/spring/boot/restful_spring_boot/config/SwaggerConfig.java
+++ src/main/java/com/restful/spring/boot/restful_spring_boot/config/SwaggerConfig.java
@@ -1,27 +1,97 @@
 package com.restful.spring.boot.restful_spring_boot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hibernate.validator.internal.util.CollectionHelper.newArrayList;
 
 @Configuration
 @EnableSwagger2
+@ComponentScan(basePackages = "com.restful.spring.boot.restful_spring_boot.controller")
 public class SwaggerConfig {
+
+    @Value("${config.oauth2.accessTokenUri}")
+    private String accessTokenUri;
+
+
+    public static final String securitySchemaOAuth2 = "oauth2schema";
+    public static final String authorizationScopeGlobal = "global";
+    public static final String authorizationScopeGlobalDesc ="accessEverything";
 
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.restful.spring.boot.restful_spring_boot.controller"))
-                .paths(PathSelectors.regex("/.*"))
-                .build().apiInfo(apiEndPointsInfo());
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Arrays.asList(securitySchema(), apiKey(), apiCookieKey()))
+                .apiInfo(apiEndPointsInfo());
     }
+
+    @Bean
+    public SecurityScheme apiKey() {
+        return new ApiKey(HttpHeaders.AUTHORIZATION, "apiKey", "header");
+    }
+
+    @Bean
+    public SecurityScheme apiCookieKey() {
+        return new ApiKey(HttpHeaders.COOKIE, "apiKey", "cookie");
+    }
+
+    private OAuth securitySchema() {
+
+        List<AuthorizationScope> authorizationScopeList = newArrayList();
+        authorizationScopeList.add(new AuthorizationScope("read", "read all"));
+        authorizationScopeList.add(new AuthorizationScope("write", "access all"));
+
+        List<GrantType> grantTypes = newArrayList();
+        GrantType passwordCredentialsGrant = new ResourceOwnerPasswordCredentialsGrant(accessTokenUri);
+        grantTypes.add(passwordCredentialsGrant);
+
+        return new OAuth("oauth2", authorizationScopeList, grantTypes);
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth())
+                .build();
+    }
+
+
+
+    private List<SecurityReference> defaultAuth() {
+
+        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[3];
+        authorizationScopes[0] = new AuthorizationScope("read", "read all");
+        authorizationScopes[1] = new AuthorizationScope("trust", "trust all");
+        authorizationScopes[2] = new AuthorizationScope("write", "write all");
+
+        return Collections.singletonList(new SecurityReference("oauth2", authorizationScopes));
+    }
+
+    @Bean
+    public SecurityConfiguration security() {
+        return new SecurityConfiguration
+                ("client", "secret", "", "", "Bearer access token", ApiKeyVehicle.HEADER, HttpHeaders.AUTHORIZATION,"");
+    }
+
 
     private ApiInfo apiEndPointsInfo(){
         return new ApiInfoBuilder()
